13887d83b3f020bc7d495c654d45bb1d620ced92
Nico769 <ncl.lnd769@gmail.com>
2019-01-17 22:40:18 +0100

    Fix logs path for matmul runner

--- core/shscripts/mat_mul_runner.sh
+++ core/shscripts/mat_mul_runner.sh
@@ -2,7 +2,7 @@
 for MATDIM in 256 512 1024; do
         for TEST in 1 2 3 4; do
             for NODES in 1 2 4 8; do
-                mpiexec -f $HOME/cloud/machinefile -n $NODES python3 $HOME/cloud/core/algos/matmul.py $MATDIM >> mpi.$NODES.$MATDIM.log
+                mpiexec -f $HOME/cloud/machinefile -n $NODES python3 $HOME/cloud/core/algos/matmul.py $MATDIM >> $HOME/cloud/core/logparsers/logs/mpi.$NODES.$MATDIM.log
 			done
 		done
 	done
